f0d988c50865fb8eb49a9a452c68fe368274bc05
mochrira <mochrira@gmail.com>
2023-03-19 23:00:11 +0700

    pass BASE_URL to php environment variables

--- 7.4.33-apache/entrypoint.sh
+++ 7.4.33-apache/entrypoint.sh
@@ -1,5 +1,10 @@
 PRODUCTION=${PRODUCTION:-NO}
 REINSTALL_COMPOSER=${REINSTALL_COMPOSER:-NO}
+BASE_URL=${BASE_URL:-}
+
+if [ $BASE_URL != "" ]; then
+    sudo /bin/sh -c "echo \"env[BASE_URL]=$BASE_URL\" >> /etc/php7/php-fpm.d/www.conf";
+fi
 
 if [ -f "composer.json" ]; then
     if [[ ! -d "vendor" || $REINSTALL_COMPOSER == "YES" ]]; then
