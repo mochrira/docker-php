f6c47d40d47fe33bca8e1ff0b2ef0f32ada8d7b5
mochrira <mochrira@gmail.com>
2023-03-20 22:05:35 +0700

    publish environment variables into php-fpm

--- 7.4.33-apache/entrypoint.sh
+++ 7.4.33-apache/entrypoint.sh
@@ -1,20 +1,23 @@
-PRODUCTION=${PRODUCTION:-NO}
-REINSTALL_COMPOSER=${REINSTALL_COMPOSER:-NO}
-BASE_URL=${BASE_URL:-}
+echo -e "[www]" | sudo tee /etc/php7/php-fpm.d/docker.env.conf > /dev/null;
 
-if [ $BASE_URL != "" ]; then
-    sudo /bin/sh -c "echo \"env[BASE_URL]=$BASE_URL\" >> /etc/php7/php-fpm.d/www.conf";
-fi
+PRODUCTION=${PRODUCTION:-NO}
+[ $PRODUCTION == "YES" ] && echo -e "env[PRODUCTION]=YES" | sudo tee -a /etc/php7/php-fpm.d/docker.env.conf > /dev/null;
+
+REINSTALL_COMPOSER=${REINSTALL_COMPOSER:-NO}
+[ $REINSTALL_COMPOSER == "YES" ] && echo -e "env[REINSTALL_COMPOSER]=YES" | sudo tee -a /etc/php7/php-fpm.d/docker.env.conf > /dev/null;
+
+BASE_URL=${BASE_URL}
+[ ! -z "$BASE_URL" ] && echo -e "env[BASE_URL]=$BASE_URL" | sudo tee -a /etc/php7/php-fpm.d/docker.env.conf > /dev/null;
 
 if [ -f "composer.json" ]; then
     if [[ ! -d "vendor" || $REINSTALL_COMPOSER == "YES" ]]; then
-        if [ $PRODUCTION == "YES" ]; then sudo composer install; else composer install; fi
+        if [ $PRODUCTION == "YES" ]; then sudo -E composer install; else composer install; fi
     fi
 fi
 
 if [ -d "./.docker" ]; then
     if [ -f "./.docker/init.php" ]; then
-        if [ $PRODUCTION == "YES" ]; then sudo php -r "require './.docker/init.php';"; else php -r "require './.docker/init.php';"; fi
+        if [ $PRODUCTION == "YES" ]; then sudo -E php -r "require './.docker/init.php';"; else php -r "require './.docker/init.php';"; fi
     fi
 fi
 
--- 7.4.33-nginx/example/index.php
+++ 7.4.33-nginx/example/index.php
@@ -2,6 +2,9 @@
 
 require 'vendor/autoload.php';
 
+echo getenv('PRODUCTION');
+echo getenv('BASE_URL');
+
 Selvi\Route::get('/rewrite', function () {
     echo 'Rewrite works perfectly'; 
 });
